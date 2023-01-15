465c8a580930101c76716a2f1467507ad4affffd
bilalabdelkadir <bilalbinabdelkadir>
2023-01-15 15:52:36 +0300

    added light and dark mode and finished the navigation section

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Coins from "./components/Coins";
-import Coin from "./components/coin";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
-  const [coins, setcoins] = useState([]);
-  const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-
-  useEffect(() => {
-    const getData = async () => {
-      const response = await axios.get(url);
-      setcoins(response.data);
-    };
-    getData();
-  }, []);
-
   return (
-    <div className="bg-gray-800 text-gray-100">
-      <Routes>
-        <Route path="/" element={<Coins coins={coins} />} />
-        <Route path="/coin/:id" element={<Coin />} />
-      </Routes>
-    </div>
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const Navbar = () => {
+  const [Nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!Nav);
+  };
+
+  const navClass = `md:hidden fixed left-0 top-20 flex flex-col items-center justify-between w-full h-[90%] bg-primary ease-in duration-200 z-10`;
+
+  return (
+    <div className="rounded-div flex items-center justify-between h-20 font-bold">
+      <Link to="/">
+        <h1 className="text-2xl ">Zee-Crypto</h1>
+      </Link>
+      <div className="hidden md:block">
+        <ThemeToggle />
+      </div>
+      <div className="hidden md:block">
+        <Link className="p-4 hover:text-accent" to="/singin">
+          Sign In
+        </Link>
+        <Link
+          className="bg-button text-btnText px-5 py-2 rounded-xl hover:rounded-none ease-in-out duration-100 ml-2 "
+          to="/singout"
+        >
+          Sign out
+        </Link>
+      </div>
+      <div className="block md:hidden cursor-pointer z-50">
+        {!Nav ? (
+          <AiOutlineMenu onClick={handleNav} size={30} />
+        ) : (
+          <AiOutlineClose onClick={handleNav} size={30} />
+        )}
+      </div>
+      {/* mobile nav */}
+      <div className={Nav ? navClass : "fixed left-[-100%]"}>
+        <ul className="w-full p-4">
+          <li className="border-b py-6">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="border-b py-6">
+            <Link to="/account">Account</Link>
+          </li>
+          <li className=" py-6">
+            <ThemeToggle />
+          </li>
+        </ul>
+        <div className="flex flex-col p-4 w-full">
+          <Link to="/signin">
+            <button className="w-full my-2 p-3 bg-primary text-primary border-secondary rounded-2xl shadow-2xl ">
+              Sing In
+            </button>
+          </Link>
+          <Link to="/signout">
+            <button className="w-full p-3 my-2 bg-button text-btnText">
+              Sing Out
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
