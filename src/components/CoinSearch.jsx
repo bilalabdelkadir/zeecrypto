c5e06edf5c10a60a485cd6cbfe592fc29eef3ab2
bilalabdelkadir <bilalbinabdelkadir>
2023-01-15 18:45:53 +0300

    added trending component

--- src/components/CoinSearch.jsx
+++ src/components/CoinSearch.jsx
@@ -4,15 +4,15 @@ import CoinItems from "./CoinItems";
 const CoinSearch = ({ coins }) => {
   const [SearchText, setSearchText] = useState("");
   return (
-    <div>
-      <div className="rounded-div my-4 flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
+    <div className="rounded-div my-4">
+      <div className=" flex flex-col md:flex-row justify-between pt-4 pb-6 text-center md:text-right">
         <h1 className="text-2xl font-bold my-2 ">Search Crypto</h1>
         <form>
           <input
             onChange={(e) => {
               setSearchText(e.target.value);
             }}
-            className="outline-none w-full bg-primary border-input px-4 py-2 rounded-2xl shadow-xl"
+            className="outline-none w-full bg-primary border-input px-4 py-2  shadow-xl"
             type="text"
             placeholder="Search A Coin"
           />
